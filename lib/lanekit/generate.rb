749131ce1e5a2495f98dc4b36d3d913125670199
Larry Aasen <larryaasen@gmail.com>
2014-07-25 00:05:16 -0400

    -added 'generate flurry' command that integrates Flurry SDK into project and adds Flurry startSession code to delegate.

--- lib/lanekit/generate.rb
+++ lib/lanekit/generate.rb
@@ -19,6 +19,34 @@ class LaneKit::Generate < Thor
     LaneKit.add_pod_to_podfile(pod_name)
   end
 
+  desc "flurry", "Adds the Flurry pod to the Podfile and updates the LKAppDelegate"
+  def flurry()
+    lanefile = LaneKit::Lanefile.new
+    lanefile_error = LaneKit.validate_lanefile(lanefile)
+    if lanefile_error
+      say lanefile_error, :red
+      return
+    end
+    controllers_folder = LaneKit.controllers_folder(lanefile)
+    app_delegate_file = "#{controllers_folder}/LKAppDelegate.m"
+
+    update_file_add_line(app_delegate_file, 'imports', ['#import "Flurry.h"'])
+
+    text_lines = [
+                  '    {',
+                  '        // Load the Flurry SDK',
+                  '        // TODO: replace your_flurry_key below with your Flurry Key',
+                  '        [Flurry startSession:@"your_flurry_key"];',
+                  '        [Flurry logEvent:@"applicationDidFinishLaunching"];',
+                  '    }',
+                  ''
+                  ]
+    update_file_add_line(app_delegate_file, 'app-did-finish-loading', text_lines)
+
+    pod_name = 'FlurrySDK'
+    LaneKit.add_pod_to_podfile(pod_name)
+  end
+
   no_tasks do
     def source_paths
       LaneKit.template_folders
@@ -27,6 +55,44 @@ class LaneKit::Generate < Thor
     def update_xcode_project
       xcworkspace_path = ""
       Xcodeproj::Workspace.new_from_xcworkspace(xcworkspace_path)
+    end
+
+    def update_file_add_line(filename, template_tag, text_lines)
+      added_lines = false
+  
+      if File.exists?(filename)
+        template_tag = "{" + template_tag + "}\n"
+        
+        insert_into_file filename, :after => template_tag do
+          text_lines.join("\n") + "\n"
+        end
+  
+        added_lines = true
+      end
+  
+      ## Open the file to be updated
+      #if File.exists?(filename)
+      #  found = false
+      #  File.readlines(filename).each do |line|
+      #    found_template = line.grep(/#{template_tag}/)
+      #    # If we found a tag
+      #    if found_template.length > 0
+      #      # If we are inside a tag
+      #      if found
+      #        # We are now finished with the tag
+      #        break
+      #      end
+      #      
+      #      found = true
+      #    end
+      #  end
+      #  return added_lines
+      #else
+      #  puts "The file '#{filename}' does not exist"
+      #  return added_lines
+      #end
+      
+      return added_lines
     end
   end
 
--- lib/template/lanekit-ios-project/lanekit-ios-project/lanekit-ios-project/Controllers/LKAppDelegate.m
+++ lib/template/lanekit-ios-project/lanekit-ios-project/lanekit-ios-project/Controllers/LKAppDelegate.m
@@ -3,16 +3,18 @@
 //
 
 #import "LKAppDelegate.h"
+// Used by LaneKit. Do not remove this: {imports}
 
 @implementation LKAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  UIViewController *controller = UIViewController.new;
-  [(UINavigationController *)self.window.rootViewController pushViewController:controller animated:NO];
-  controller.navigationItem.title = @"lanekit-ios-project";
-
-  return YES;
+    UIViewController *controller = UIViewController.new;
+    [(UINavigationController *)self.window.rootViewController pushViewController:controller animated:NO];
+    controller.navigationItem.title = @"lanekit-ios-project";
+    
+    // Used by LaneKit. Do not remove this: {app-did-finish-loading}
+    return YES;
 }
 
 @end
